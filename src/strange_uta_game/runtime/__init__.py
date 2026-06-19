bdf7efb332c253245c1f2728af53b23319aaebd3
zesty_fox <zhs24@mails.tsinghua.edu.cn>
2026-06-20 01:48:47 +0800

    feat: add runtime capability registry

---
+++ src/strange_uta_game/runtime/__init__.py
@@ -0,0 +1,5 @@
+"""Shared runtime bootstrap primitives."""
+
+from .capabilities import Capability, CapabilityRegistry, CapabilityStatus
+
+__all__ = ["Capability", "CapabilityRegistry", "CapabilityStatus"]
